64ae446ee9d060e4e421deda61d903e9e5d45ca6
chispalex89 <chispalex89@gmail.com>
2019-11-08 22:25:00 -0600

    Add project files.

---
+++ MvcClient/Controllers/WeatherForecastController.cs
@@ -0,0 +1,50 @@
+﻿
+
+using MvcClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+namespace MvcClient.Controllers
+{
+    public class WeatherForecastController : Controller
+    {
+        // GET: WeatherForcast
+        public ActionResult Index()
+        {
+            IEnumerable<WeatherForecast> weather = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:55781/");
+                //HTTP GET
+                var responseTask = client.GetAsync("weatherforecast");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsStringAsync();
+                    readTask.Wait();
+                    weather = JsonConvert.DeserializeObject<IList<WeatherForecast>>(readTask.Result);
+                    
+                }
+                else
+                {
+                    weather = Enumerable.Empty<WeatherForecast>();
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+            }
+            return View(weather);
+        }
+    }
+}
+
+/*
+ 
+ 
+     */
